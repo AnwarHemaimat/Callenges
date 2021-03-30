2f61054871d2da115644a01e9ffd5ab4b60e60ab
AnwarHemaimat <Anwarhomaimat@gmail.com>
2021-03-30 13:34:48 +0300

    challenge 1

--- challenge-1-solution.js
+++ challenge-1-solution.js
@@ -1,9 +1,9 @@
 'use strict'
 const square= (Element) => Element ** 2;
-const a= [1,2,3,4,5];
-const a2=a.map(square);
+const numberArray= [1,2,3,4,5];
+const numberSquare=numberArray.map(square);
 
 function myFunction() {
     document.getElementById('demo').innerHTML='The numbers are: 1 ,2 ,3 ,4 ,5'+'<br>'+
-     'The square of numbere are: '+a2;
+     'The square of numbere are: '+numberSquare;
 }
